f7632cb2e7868effb2532ad21167ddd90eb49aaf
Francis Quintal <francisquintal.fq@gmail.com>
2016-12-10 20:16:45 -0800

    Cleaned up project to only hold backend, added full CRUD to controller and model, edited home page to redirect to front end, edited datadump to auto increment id's

--- application/models/Supplies.php
+++ application/models/Supplies.php
@@ -8,51 +8,47 @@
 
 class Supplies extends CI_Model{
     
-    // var $data = array(
-	// 	array('id' => '0',  'name' => 'Waffle Cone',    'price' => 50,  'type' => 'container',  'perBox' => 60, 'onHand' => 10),
-	// 	array('id' => '1',  'name' => 'Regular Cone',   'price' => 25,  'type' => 'container',  'perBox' => 60, 'onHand' => 10),
-    //     array('id' => '2',  'name' => 'Plastic Cup',    'price' => 15,  'type' => 'container',  'perBox' => 50, 'onHand' => 10),
-    //     array('id' => '3',  'name' => 'Vanilla',        'price' => 20,  'type' => 'icecream',  'perBox' => 45, 'onHand' => 10),
-    //     array('id' => '4',  'name' => 'Strawberry',     'price' => 20,  'type' => 'icecream',  'perBox' => 45, 'onHand' => 10),
-    //     array('id' => '5',  'name' => 'Chocolate',      'price' => 20,  'type' => 'icecream',  'perBox' => 45, 'onHand' => 10),
-    //     array('id' => '6',  'name' => 'Mint',           'price' => 25,  'type' => 'icecream',  'perBox' => 45, 'onHand' => 10),
-    //     array('id' => '7',  'name' => 'Maple',          'price' => 25,  'type' => 'icecream',  'perBox' => 45, 'onHand' => 10),
-    //     array('id' => '8',  'name' => 'Orange',         'price' => 25,  'type' => 'icecream',  'perBox' => 45, 'onHand' => 10),
-    //     array('id' => '9',  'name' => 'Sprinkles',      'price' => 18,  'type' => 'garnish',  'perBox' => 120, 'onHand' => 10),
-    //     array('id' => '10', 'name' => 'Walnuts',        'price' => 35,  'type' => 'garnish',  'perBox' => 85, 'onHand' => 0),
-    //     array('id' => '11', 'name' => 'Chocolate Chips','price' => 13,  'type' => 'garnish',  'perBox' => 100, 'onHand' => 10),
-	// );
-
 	// Constructor
 	public function __construct() {
 		parent::__construct();
 	}
 
+	//CREATE****************************
+	public function create($name,$price,$type,$perBox,$onHand){
+		
+		$item = $this->db->query("INSERT INTO supplies (name,price,type,PerBox,OnHand) VALUES ('".$name."', ".$price.", '".$type."', ".$perBox.", ".$onHand.")");
+
+		//update Log
+		$data = 'CREATED: '.$name . date(" 	Y/m/d") . "\n";
+		if ( !write_file(APPPATH.'/logs/inventory.txt', $data, 'a')){
+		     echo 'Unable to write the file';
+		}
+		return 0;
+	}
+	//READ********************************
+	//GET ALL
 	public function all(){
 		$item = $this->db->query('Select * from supplies');
 		return $item->result_array();
 	}
+	//GET ITEM
 	public function get($id){
 		$item = $this->db->query('Select * from supplies where id = '. $id);
 		return $item->result();
 	}
-
-	// get number of ingredient on hand
+	//GET ONHAND
 	public function getOnHand($id) {
 		$item = $this->db->query('Select * from supplies where id = '. $id);
 		return $item->row()->onHand;
-
 	}
-
-	//get box size of ingredient
+	//GET BOX SIZE
 	public function getPerBox($id) {
 		$item = $this->db->query('Select * from supplies where id = '. $id);
 		return $item->row()->perBox;
 	}
-
-	//receive an item
+	//UPDATE*********************************8
+	//RECEIVE ITEM
 	public function add($id, $numOfBoxes){
-		
 		$item = $this->db->query('Select * from supplies where id = '. $id);
 		//check if id valid
 		if($item->row() == null){
@@ -72,9 +68,7 @@ class Supplies extends CI_Model{
 		}
 		return 0;
 	}
-
-	// this could be moded to take in a recipe?
-	//use item from stocklist
+	//USE AN ITEM (REDUCE ONHAND)
 	public function use($id, $numTaken){
 		$item = $this->db->query('SELECT * FROM supplies WHERE id = '. $id);
 		//check if id valid
@@ -92,4 +86,60 @@ class Supplies extends CI_Model{
 		}
 		return 0;
 	}
+	//UPDATE price of item
+	public function priceChange($id, $newPrice){
+		//check if id valid
+		$item = $this->db->query('SELECT * FROM supplies WHERE id = '. $id);
+		if($item->row() == null){
+			return 1;
+		}
+
+		$this->db->query('UPDATE supplies SET price = ' . $newPrice . ' WHERE id = '. $id);
+
+		//Update Log
+		$data = 'EDITED: '.$item->row()->name.' price changed to ' . $newPrice .' on ' . date("Y/m/d") . "\n";
+		if ( !write_file(APPPATH.'/logs/inventory.txt', $data, 'a')){
+		     echo 'Unable to write the file';
+		}
+		return 0;
+	}
+
+	//UPDATE number of items per box
+	public function perBoxChange($id, $newAmount){
+		
+		//check if id valid
+		$item = $this->db->query('SELECT * FROM supplies WHERE id = '. $id);
+		if($item->row() == null){
+			return 1;
+		}
+		
+		$this->db->query('UPDATE supplies SET perBox = ' . $newAmount . ' WHERE id = '. $id);
+
+		//Update Log
+		$data = 'EDITED: '.$item->row()->name.' price changed to ' . $newPrice .' on ' . date("Y/m/d") . "\n";
+		if ( !write_file(APPPATH.'/logs/inventory.txt', $data, 'a')){
+		     echo 'Unable to write the file';
+		}
+		return 0;
+	}
+
+	//DELETE***************************
+	public function delete($id){
+		//check if id valid
+		$item = $this->db->query('SELECT * FROM supplies WHERE id = '. $id);
+		if($item->row() == null){
+			return 1;
+		}
+
+		$this->db->query('DELETE from supplies WHERE id = '. $id);
+
+		//Update Log
+		$data = 'DELETED: '.$item->row()->name.' deleted from list on ' . date("Y/m/d") . "\n";
+		if ( !write_file(APPPATH.'/logs/inventory.txt', $data, 'a')){
+		     echo 'Unable to write the file';
+		}
+		return 0;
+	}
+
+
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,25 +1,6 @@
 <div id="body" class="text-center">
-    <h1>Assignment #1 From Comp 4711</h1>
+    <h1>Assignment #2 From Comp 4711</h1>
 
-    <table class="table">
-    <form action="">
-        <tr>
-            <th>Amount spent purchasing inventory</th>
-            <td>{inventorycost}</td>
-        </tr>
-        <tr>
-            <th>Amount received from sales</th>
-            <td>{salesamount}</td>
-        </tr>
-        <tr>
-            <th>Cost of Sales Ingredients consumed</th>
-            <td>{costofingredientsused}</td>
-        </tr>
-           
-    </form>
-</table>
-    <br>
-    
-        
+    <h1>Thanks For coming but please visit our store instead!</h1>
 </div>
 
